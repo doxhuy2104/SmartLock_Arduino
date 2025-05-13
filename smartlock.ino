dc270a6168743ef55bfa9aee8529cd65a2ec9b22
Huy <doxhuy2104@gmail.com>
2025-05-13 21:35:49 +0700

    config lock

--- smartlock.ino
+++ smartlock.ino
@@ -1,11 +1,15 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include<Keypad.h>
+#include <Keypad.h>
 #include <WiFi.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Firebase_ESP_Client.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
 
 //Oled
 #define SS_PIN 5
@@ -16,54 +20,63 @@
 
 //firebase
 /* 1. Define the WiFi credentials */
-#define WIFI_SSID "10/149/211 KT"
-#define WIFI_PASSWORD "03082002"
+// #define WIFI_SSID "Z9Tb"
+// #define WIFI_PASSWORD "21102004"
 
 /* 2. Define the API Key */
 #define API_KEY "AIzaSyA5jxcI0Dle0iN5pJY-aqw8j7qtZVyiE1M"
 
 /* 3. Define the RTDB URL */
-#define DATABASE_URL "https://smartlock-25056-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+#define DATABASE_URL "https://smartlock-25056-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 /* 4. Define the user Email and password that alreadey registerd or added in your project */
 #define USER_EMAIL "doxhuy2104@gmail.com"
 #define USER_PASSWORD "wqqpwppr2104"
 
+//Relay
+#define RELAY_PIN 15
+
+//Bluetooth
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
 //
-int x1=0, x2=-128;
+bool isDone = false;
+
+int x1 = 0, x2 = -128;
 int count = 0;
 String input = "";
-int tryCount=5;
-int lockState=1;
+int tryCount = 5;
+int lockState = 1;
 
-String PIN="2104";
+String PIN = "2104";
 
 byte nuidPICC[4];
 
-byte listRFID[][4]= {
-  {0x94,0x64,0x0F,0x02}
+byte listRFID[][4] = {
+  { 0x94, 0x64, 0x0F, 0x02 }
 };
 
 int numCards = sizeof(listRFID) / sizeof(listRFID[0]);
 
-MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522 rfid(SS_PIN, RST_PIN);  // Instance of the class
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //four columns
+const byte ROWS = 4;  //four rows
+const byte COLS = 3;  //four columns
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'*','0','#'}
+  { '1', '2', '3' },
+  { '4', '5', '6' },
+  { '7', '8', '9' },
+  { '*', '0', '#' }
 };
-byte rowPins[ROWS] = {13, 12, 14, 27}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {26, 25, 33}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = { 13, 12, 14, 27 };  //connect to the row pinouts of the keypad
+byte colPins[COLS] = { 26, 25, 33 };      //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 // Define Firebase Data object
 FirebaseData fbdo;
@@ -73,35 +86,119 @@ FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
 
-void setup() {
-    Serial.begin(115200);
-    SPI.begin(); // Init SPI bus
-    rfid.PCD_Init(); // Init MFRC522 
+String WIFI_SSID = "";
+String WIFI_PASSWORD = "";
+String uid = "";
+String id = "";
 
-    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-        Serial.println("Không tìm thấy màn hình OLED");
-        while (true);
+//CallBacks function of bluetooth
+class MyCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    String value = pCharacteristic->getValue();
+    if (value.length() > 0) {
+      String received = "";
+      for (int i = 0; i < value.length(); i++) {
+        received += value[i];
+      }
+      Serial.print("received: ");
+      Serial.println(received);
+      if (value[0] == 'n') {
+        for (int i = 1; i < value.length(); i++) {
+          WIFI_SSID += value[i];
+        }
+        Serial.print("WIFI_SSID: ");
+        Serial.println(WIFI_SSID);
+      } else if (value[0] == 'p') {
+        for (int i = 1; i < value.length(); i++) {
+          WIFI_PASSWORD += value[i];
+        }
+        Serial.print("WIFI_PASSWORD: ");
+        Serial.println(WIFI_PASSWORD);
+        WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+        Serial.print("Connecting to Wi-Fi");
+        int count = 0;
+        while (WiFi.status() != WL_CONNECTED) {
+          Serial.print(".");
+          delay(500);
+          count++;
+          if (count >= 18) {
+            WiFi.disconnect(true);
+            pCharacteristic->setValue("WIFI_FAILED");
+            pCharacteristic->notify();
+            WIFI_SSID.clear();
+            WIFI_PASSWORD.clear();
+            return;
+          }
+        }
+        Serial.println();
+        Serial.print("Connected with IP: ");
+        Serial.println(WiFi.localIP());
+        Serial.println();
+        pCharacteristic->setValue("Connected");
+        pCharacteristic->notify();
+      } else if (value[0] == 'u') {
+        for (int i = 1; i < value.length(); i++) {
+          uid += value[i];
+        }
+        Serial.print("uid: ");
+        Serial.println(uid);
+      } else if (value[0] == 'i') {
+        for (int i = 1; i < value.length(); i++) {
+          id += value[i];
+        }
+        Serial.print("id: ");
+        Serial.println(id);
+        pCharacteristic->setValue("Done");
+        pCharacteristic->notify();
+        BLEDevice::deinit();
+        isDone = true;
+      }
     }
-
-    display.clearDisplay();
-
-    display.setTextSize(1); 
-    display.setTextColor(SSD1306_WHITE); 
-      display.setCursor(20,18);
-      display.print("Nhap mat khau");
-      display.display();
-
-      WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-      Serial.print("Connecting to Wi-Fi");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(300);
   }
-  Serial.println();
-  Serial.print("Connected with IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.println();
+};
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();      // Init SPI bus
+  rfid.PCD_Init();  // Init MFRC522
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println("Không tìm thấy màn hình OLED");
+    while (true)
+      ;
+  }
+
+  //bluetooth
+  BLEDevice::init("My Lock");
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic =
+    pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+
+  pCharacteristic->setCallbacks(new MyCallbacks());
+
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();
+  pAdvertising->start();
+
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(20, 18);
+  display.print("Nhap mat khau");
+  display.display();
+  while (!isDone) {
+    delay(100);
+  }
+
 
   /* Assign the api key (required) */
   config.api_key = API_KEY;
@@ -136,7 +233,7 @@ void loop() {
   getFirebaseData();
 }
 
-void oledDisplay(const char *text){
+void oledDisplay(const char *text) {
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(1);
@@ -144,22 +241,22 @@ void oledDisplay(const char *text){
   display.println(text);
   display.display();
 }
-void mainScreen(){
-display.clearDisplay();
-      display.setTextSize(1); 
-      display.setTextColor(SSD1306_WHITE); 
-      display.setCursor(20,18);
-      display.print("Nhap mat khau");
-      display.display();
+void mainScreen() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(20, 18);
+  display.print("Nhap mat khau");
+  display.display();
 }
 
-void readRFID(){
-// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
+void readRFID() {
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent())
     return;
 
   // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
+  if (!rfid.PICC_ReadCardSerial())
     return;
 
   Serial.print(F("PICC type: "));
@@ -167,24 +264,19 @@ void readRFID(){
   Serial.println(rfid.PICC_GetTypeName(piccType));
 
   // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI && piccType != MFRC522::PICC_TYPE_MIFARE_1K && piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
     Serial.println(F("Your tag is not of type MIFARE Classic."));
     return;
   }
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || rfid.uid.uidByte[1] != nuidPICC[1] || rfid.uid.uidByte[2] != nuidPICC[2] || rfid.uid.uidByte[3] != nuidPICC[3]) {
     Serial.println(F("A new card has been detected."));
 
     // Store NUID into nuidPICC array
     for (byte i = 0; i < 4; i++) {
       nuidPICC[i] = rfid.uid.uidByte[i];
     }
-   
+
     Serial.println(F("The NUID tag is:"));
     Serial.print(F("In hex: "));
     printHex(rfid.uid.uidByte, rfid.uid.size);
@@ -192,14 +284,13 @@ void readRFID(){
     Serial.print(F("In dec: "));
     printDec(rfid.uid.uidByte, rfid.uid.size);
     Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
+  } else Serial.println(F("Card read previously."));
 
-// display
+  // display
   // display.clearDisplay();
   // display.setCursor(0,0);
   // display.println(F("RFID Detected!"));
-  
+
   // display.print(F("HEX: "));
   // for (byte i = 0; i < rfid.uid.size; i++) {
   //   display.print(rfid.uid.uidByte[i], HEX);
@@ -209,24 +300,22 @@ void readRFID(){
     Serial.println(F("Thẻ hợp lệ!"));
 
     oledDisplay("Mo khoa thanh cong!");
-        tryCount=5;
-        input="";
-        delay(5000);
-        display.clearDisplay();
+    tryCount = 5;
+    input = "";
+    delay(5000);
+    display.clearDisplay();
 
-      display.setTextSize(1); 
-      display.setTextColor(SSD1306_WHITE); 
-      display.setCursor(20,18);
-      display.print("Nhap mat khau");
-      display.display();
-  }
-  else{
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(20, 18);
+    display.print("Nhap mat khau");
+    display.display();
+  } else {
     oledDisplay("The sai!");
     delay(2000);
     mainScreen();
-
   }
-  
+
   // display.println();
   // display.print(F("DEC: "));
   // for (byte i = 0; i < rfid.uid.size; i++) {
@@ -243,62 +332,61 @@ void readRFID(){
   rfid.PCD_StopCrypto1();
 }
 
-void enterPIN(){
-char customKey = customKeypad.getKey();
-  
-  if (customKey){
-    if(count==0){
-      display.clearDisplay();
-      display.setCursor(20,18);
-          display.setTextSize(2); 
+void enterPIN() {
+  char customKey = customKeypad.getKey();
 
-      }
+  if (customKey) {
+    if (count == 0) {
+      display.clearDisplay();
+      display.setCursor(20, 18);
+      display.setTextSize(2);
+    }
     display.print("* ");
     display.display();
-    input+=customKey;
+    input += customKey;
     Serial.println(customKey);
     count++;
-    if(count==4){
-      
-      count=0;
-      if(input==PIN){
+    if (count == 4) {
+
+      count = 0;
+      if (input == PIN) {
         oledDisplay("Mo khoa thanh cong!");
-        tryCount=5;
-        input="";
+        tryCount = 5;
+        input = "";
         delay(5000);
         mainScreen();
-      }
-      else{
-        input="";
+      } else {
+        input = "";
         tryCount--;
         oledDisplay("Sai mat khau");
         oledDisplay(("Ban con " + String(tryCount) + " lan thu").c_str());
-        if(tryCount==0){
+        if (tryCount == 0) {
           delay(300000);
-          tryCount=5;
+          tryCount = 5;
         }
       }
     }
-    
   }
 }
 
-void getFirebaseData(){
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0))
-  {
+void getFirebaseData() {
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 1000 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
 
-   if(Firebase.RTDB.getInt(&fbdo, "/lock/state", &lockState)){
-    if(!lockState){
-      oledDisplay("Mo khoa thanh cong!");
+    if (Firebase.RTDB.getInt(&fbdo, uid + "/" + id + "/state", &lockState)) {
+      if (!lockState) {
+        oledDisplay("Mo khoa thanh cong!");
         delay(5000);
         mainScreen();
-        lockState=1;
-        Firebase.RTDB.setInt(&fbdo,"/lock/state", 1);
-    } 
-   }else{
-    // Serial.println(fbdo.errorReason().c_str());
-   }
+        lockState = 1;
+        Firebase.RTDB.setInt(&fbdo, uid + "/" + id + "/state", 1);
+      }
+    } else {
+      Serial.println(fbdo.errorReason().c_str());
+    }
+    if (Firebase.RTDB.get(&fbdo, "XTYv2rwFJ3ZV6390OIJBlmzZSSk1/lock1/PIN")) {
+      PIN = fbdo.stringData();
+    }
   }
 }
 
@@ -319,13 +407,11 @@ void printDec(byte *buffer, byte bufferSize) {
   }
 }
 
-bool check(byte *id){
-  for(int i=0;i<numCards;i++){
-    if (id[0]==listRFID[i][0]&&id[1]==listRFID[i][1]&&id[2]==listRFID[i][2]&&id[3]==listRFID[i][3]) {
+bool check(byte *id) {
+  for (int i = 0; i < numCards; i++) {
+    if (id[0] == listRFID[i][0] && id[1] == listRFID[i][1] && id[2] == listRFID[i][2] && id[3] == listRFID[i][3]) {
       return true;
     }
   }
   return false;
 }
-
-
